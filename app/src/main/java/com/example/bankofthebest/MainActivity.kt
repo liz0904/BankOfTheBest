a8307bb9be4ab7a015a2390fbb8c0487fcf4b0f5
liz0904 <liz09045@naver.com>
2022-08-07 15:54:56 +0900

    [add] font & [add] main activity

--- app/src/main/java/com/example/bankofthebest/MainActivity.kt
+++ app/src/main/java/com/example/bankofthebest/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.bankofthebest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import com.example.bankofthebest.community.MainCommuActivity
@@ -17,12 +18,12 @@ import io.realm.RealmConfiguration
 import io.realm.exceptions.RealmMigrationNeededException
 import io.realm.kotlin.createObject
 import io.realm.kotlin.where
+import org.jetbrains.anko.find
 import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
 
     // 변수 선언
-    lateinit var todoButton: ImageButton
     lateinit var btn_port : ImageButton
     lateinit var btn_awards : ImageButton
     lateinit var btn_commu : ImageButton
@@ -32,7 +33,10 @@ class MainActivity : AppCompatActivity() {
     lateinit var todoTitleTextView : TextView
     lateinit var todoSubtitleTextView : TextView
     lateinit var btn_refresh : ImageButton
-    lateinit var textView2:TextView
+    lateinit var text_user:TextView
+    lateinit var textview_money_saving:TextView
+    lateinit var btn_goto_minus: Button
+    lateinit var btn_goto_saving_minus:Button
 
     // 현재 액티비티에서 Realm 인스턴스 얻음
     // Migration 오류 발생에 대비하여 try-catch로 얻어옴
@@ -60,26 +64,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // activity_main.xml의 변수 id 연결
-        todoButton = findViewById(R.id.todoButton)
         btn_port = findViewById(R.id.btn_port)
         btn_awards = findViewById(R.id.btn_awards)
         btn_commu = findViewById(R.id.btn_commu)
-        btn_event=findViewById(R.id.btn_event)
-        btn_my=findViewById(R.id.btn_my)
+        btn_event = findViewById(R.id.btn_commercial)
         todoDateTextView = findViewById(R.id.todoDateTextView)
-        todoTitleTextView = findViewById(R.id.todoTitleTextView)
-        todoSubtitleTextView = findViewById(R.id.todoSubtitleTextView)
-        textView2=findViewById(R.id.textView2)
+        todoTitleTextView = findViewById(R.id.todoTitleTextView_Saving)
+        todoSubtitleTextView = findViewById(R.id.todoSubtitleTextView_Saving)
+        text_user = findViewById(R.id.text_user)
         btn_refresh = findViewById(R.id.btn_refresh)
+        textview_money_saving = findViewById(R.id.textview_money_saving)
+        btn_goto_minus = findViewById(R.id.btn_goto_minus)
+        btn_goto_saving_minus = findViewById(R.id.btn_goto_saving_minus)
 
         realm.beginTransaction()    // 첫 번째 트랜젝션 시작
 
         // 현재 로그인한 회원 정보의 id 값을 가져와 메인 화면에 보여줌
-        val person=realm.where<Person>().findFirst()
+        val person = realm.where<Person>().findFirst()
         if (person != null) {
-            textView2.text= person.id
-            todoTitleTextView.text="급여통장"
-            todoSubtitleTextView.text=person.money.toString()
+            text_user.text = person.id
         }
 
         // portDB에 기본 데이터 저장
@@ -89,28 +92,32 @@ class MainActivity : AppCompatActivity() {
             newItem.title = "2020 봄 카오스 강연 중 ‘ 블록체인’ 강연 듣기"
             newItem.date = "20/03/25 ~ 20/03/25"
             newItem.classification = "개인"
-            newItem.content = "블록체인의 특징과 등장 배경을 살펴본다. 그리고 블록체인의 기본적인 원리에 대하여 예를 중심으로 알아보고 블록체인과 암호화폐이의 관계에 대해 살펴보는 시간을 가졌다. "
+            newItem.content =
+                "블록체인의 특징과 등장 배경을 살펴본다. 그리고 블록체인의 기본적인 원리에 대하여 예를 중심으로 알아보고 블록체인과 암호화폐이의 관계에 대해 살펴보는 시간을 가졌다. "
             newItem.memo = "블록체인과 연관된 기술들에 대해 알아보고 블로그에 포스팅하기"
 
             val newItem1 = realm.createObject<portDB>(nextId_awards())
             newItem1.title = "리눅스 마스터 1급 자격증 취득"
             newItem1.date = "20/09/01 - 20/11/14"
             newItem1.classification = "개인"
-            newItem1.content = "리눅스 마스터 1급을 통해 리눅스 OS의 기본지식과 서버, 네트워크 관리 실무능력을 인증할 수 있는 자격증인데 올해 여름에 2급을 따고 나서 1급도 취득하고자 하여 2달 정도의 기간을 두고 공부하였다. "
+            newItem1.content =
+                "리눅스 마스터 1급을 통해 리눅스 OS의 기본지식과 서버, 네트워크 관리 실무능력을 인증할 수 있는 자격증인데 올해 여름에 2급을 따고 나서 1급도 취득하고자 하여 2달 정도의 기간을 두고 공부하였다. "
             newItem1.memo = "컴퓨터활용능력처럼 자주 있는 시험이 아니기 때문에 연초에 올라오는 시험 일정을 미리미리 확인하는게 좋다."
 
             val newItem2 = realm.createObject<portDB>(nextId_awards())
             newItem2.title = "토익 930점 독학 성공"
             newItem2.date = "21/01/02 - 21/02/02 "
             newItem2.classification = "단체(스터디)"
-            newItem2.content = "곧 있을 편입과 취업을 위해 토익 시험을 보았다. 요새는 공기업이라면 토익 점수를 본다. 듣기와 독해, 어휘 모든 부분을 보는 시험이기 때문에 처음에 감이 오지 않았었는데, 기출 문제를 풀고나서 어느 부분이 내가 취약한지 알 수 있었다. 그 부분들을 위주로 공부 하다 보니 점수가 빠른 속도로 오를 수 있었다."
+            newItem2.content =
+                "곧 있을 편입과 취업을 위해 토익 시험을 보았다. 요새는 공기업이라면 토익 점수를 본다. 듣기와 독해, 어휘 모든 부분을 보는 시험이기 때문에 처음에 감이 오지 않았었는데, 기출 문제를 풀고나서 어느 부분이 내가 취약한지 알 수 있었다. 그 부분들을 위주로 공부 하다 보니 점수가 빠른 속도로 오를 수 있었다."
             newItem2.memo = "친구들이나 지인들을 통해 교재나 학원 및 공부법에 대해 물어보면 도움이 많이 된다"
 
             val newItem3 = realm.createObject<portDB>(nextId_awards())
             newItem3.title = "안드로이드 개발 프로젝트"
             newItem3.date = "21/01/04 - 21/02/14"
             newItem3.classification = "단체"
-            newItem3.content = "안드로이드 앱 개발에 관심이 있는 친구들과 팀을 이루어 프로젝트를 진행하였다. 주제선정부터 개발, 디자인 단계까지 초반에 세부적으로 정하는 것이 중요하다. 팀원들간 역할 배분과 기능 담당을 나누어 개발을 하면 책임감을 더하면서도 부담은 덜 가질 수 있다. "
+            newItem3.content =
+                "안드로이드 앱 개발에 관심이 있는 친구들과 팀을 이루어 프로젝트를 진행하였다. 주제선정부터 개발, 디자인 단계까지 초반에 세부적으로 정하는 것이 중요하다. 팀원들간 역할 배분과 기능 담당을 나누어 개발을 하면 책임감을 더하면서도 부담은 덜 가질 수 있다. "
             newItem3.memo = "생각보다 구글링으로 해결되지 않는게 많았다. 교재를 사서 공부해도 도움이 될 것 같다."
         }
         realm.commitTransaction()   // 첫 번째 트랜젝션 종료
@@ -134,9 +141,7 @@ class MainActivity : AppCompatActivity() {
         realm2.commitTransaction()   // 두 번째 트랜젝션 종료
 
         // 이미지 버튼 클릭 시 해당하는 액티비티로 연결
-        todoButton.setOnClickListener { view ->
-            startActivity<TodoActivity>()
-        }
+
         btn_port.setOnClickListener { view ->
             startActivity<PortMainActivity>()
         }
@@ -149,8 +154,14 @@ class MainActivity : AppCompatActivity() {
         btn_event.setOnClickListener { view ->
             startActivity<EventListActivity>()
         }
-        btn_my.setOnClickListener {
+        text_user.setOnClickListener {
             startActivity<mypageMainActivity>()
+        }
+        btn_goto_minus.setOnClickListener { view ->
+            startActivity<TodoActivity>()
+        }
+        btn_goto_saving_minus.setOnClickListener {
+           //저금통을 해지하시겠습니까? 띄우고 예 하면 금액 변경
         }
     }
 
@@ -176,13 +187,18 @@ class MainActivity : AppCompatActivity() {
     // 메인 페이지에 체크리스트 목록 중
     // 날짜가 가장 빠른 데이터를 보여주는 함수
     private fun refreshTodo() {
-        val realmResult_main = realm2.where<Todo>().findAllAsync()
-        val realmResultmain1 = realmResult_main[0]
+        val realmResult_main = realm.where<Person>().findFirst()
+        val realmResultmain1 = realmResult_main
 
         if (realmResultmain1 != null) {
-            todoDateTextView.text = android.text.format.DateFormat.format("yyyy/MM/dd", realmResultmain1.date)
-            todoTitleTextView.text = realmResultmain1.title
-            todoSubtitleTextView.text = realmResultmain1.subtitle
+            todoDateTextView.text = "3333-08-1234567"
+            todoTitleTextView.text = "급여통장"
+            var saving_tmp=realmResult_main.money%1000
+            realmResult_main.money-=saving_tmp
+            realmResult_main.saving_money+=saving_tmp
+            todoSubtitleTextView.text =realmResult_main.money.toString()+"원"
+            textview_money_saving.text=realmResult_main.saving_money.toString()+"원"
         }
     }
+
 }
--- app/src/main/java/com/example/bankofthebest/login/Person.kt
+++ app/src/main/java/com/example/bankofthebest/login/Person.kt
@@ -5,6 +5,9 @@ import io.realm.RealmObject
 open class Person(var id:String="",
                   var pwd:String="",
                   var email:String="",
-                  var money:Int=0)
+                  var money:Int=101234,
+                  var account:String="",
+                  var saving_money: Int=1234
+)
     : RealmObject() {
 }
